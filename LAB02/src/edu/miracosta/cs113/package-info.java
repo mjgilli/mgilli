5a6af8982a73c294f4a349901ebd0ee4b787ded5
Matthew Gilli <mgilli1995@gmail.com>
2017-01-29 21:23:56 -0800

    initial commit & solution
    solves problem to Lab 02

---
+++ LAB02/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,33 @@
+/**
+* AUTHOR: Matthew Gilli
+* COURSE: CS 113
+* SECTION: MW 15:30-17:20
+* HOMEWORK #: 01
+* PROJECT #: Project 01
+* LAST MODIFIED: 2017-01-29 
+*/
+/**
+* DetectiveJill
+*****************************************************************************
+* PROGRAM DESCRIPTION:
+* This program solves a murder with 6 possible weapons, 10 locations, and
+* 6 murderers in 20 guesses or less
+*****************************************************************************
+* ALGORITHM:
+* 1.BEGIN
+* 2.	ASSIGN keyboard
+* 3.	OUTPUT message of number in question
+* 4.	INPUT  temp
+* 5.	CALC   numToTest
+* 6.	CALC   isPowerOfTwo
+* 7.	IF (isPowerOfTwo)
+* 8.		OUTPUT is power of two
+* 9.	ELSE
+* 10.		OUTPUT is not power of two
+* 11.	END IF
+* 29. END
+*****************************************************************************
+* ALL IMPORTED PACKAGES NEEDED AND PURPOSE:
+* Scanner= used for console input
+ */
+package edu.miracosta.cs113;
