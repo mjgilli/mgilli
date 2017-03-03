d0161f22714c0f7d26c5e27a4b5a70b42114ca22
Matthew Gilli <mgilli1995@gmail.com>
2017-03-02 19:11:07 -0800

    commit LAB07 & LAB08
    documentation included

--- LAB07/src/edu/miracosta/cs113/ArrayStack.java
+++ LAB07/src/edu/miracosta/cs113/ArrayStack.java
@@ -5,21 +5,34 @@ public class ArrayStack<T> implements StackInterface<T>
 {
 	ArrayList<T> theData;
 	
+	/** 
+	   constructor for array stack
+	*/
 	public ArrayStack()
 	{
 		theData = new ArrayList<T>();
 	}
 	
+	/** returns true or false if stack is empty
+	    @returns boolean if empty
+	*/
 	public boolean empty()
 	{
 		return theData.isEmpty();
 	}
 	
+	/** returns object at top of stack
+		@return Object at top of stack
+	*/
 	public T peek()
 	{
 		return theData.get(theData.size() - 1);
 	}
 	
+	/** removes and returns item at top of stack
+		@throws EmptyStackException
+		@return Object removed
+	*/
 	public T pop()
 	{
 		if(empty())
@@ -29,6 +42,10 @@ public class ArrayStack<T> implements StackInterface<T>
 		return theData.remove(theData.size() - 1);
 	}
 	
+	/** Adds element to top of stack
+    	@param Object to be added to stack
+    	@return Object to be added
+	*/
 	public T push(T object)
 	{
 		theData.add(object);
--- LAB07/src/edu/miracosta/cs113/StackInterface.java
+++ LAB07/src/edu/miracosta/cs113/StackInterface.java
@@ -1,13 +1,30 @@
 package edu.miracosta.cs113;
 
+import java.util.EmptyStackException;
+
 public interface StackInterface<T>
 {
+	
+	/** returns true or false if stack is empty
+    	@returns boolean if empty
+	*/
 	public boolean empty();
-
+	
+	/** returns object at top of stack
+		@return Object at top of stack
+	*/
 	public T peek();
 
+	/** removes and returns item at top of stack
+		@throws EmptyStackException
+		@return Object removed
+	*/
 	public T pop();
 	
+	/** Adds element to top of stack
+		@param Object to be added to stack
+		@return Object to be added
+	*/
 	public T push(T object);
 
 }
